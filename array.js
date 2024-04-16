59bb6e93cf4654b4f69c0a98e3ce799281a84de4
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-04-16 20:27:16 +0530

    Added arrays in JS

---
+++ array.js
@@ -0,0 +1,65 @@
+const fruits = ['kiwi','mango'];
+fruits.push("banana", "apple", "peach");
+console.log(fruits.length); // 3
+
+
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); // undefined
+
+//sort
+var a = [1,2 ,3,4,11]
+console.log(a.sort((a,b)=>a-b));
+
+//at
+console.log(fruits.at(1))
+
+//push and pop
+
+fruits.push('papaya');
+console.log(fruits.pop());
+
+//loops
+
+let arr = ["Apple", "Orange", "Pear"];
+
+//for loop
+for (let i = 0; i < arr.length; i++) {
+  console.log( arr[i] );
+}
+
+//for of loop
+for (let fruit of arr) {
+  console.log( fruit );
+}
+
+//for in loop
+
+for (let key in arr) {
+  console.log( arr[key] ); 
+}
+
+//splice
+
+let numArr = [20, 30, 40, 50];
+
+
+// Delete 3 elements starting from index 1
+numArr.splice(1, 3);
+
+// Insert 80, 90, 100 at starting index 1
+numArr.splice(1, 0, 3, 4, 5);
+
+console.log(numArr);
+
+
+// Declare and initialize array
+let strArr = ["amit", "sumit", "anil"];
+
+// Delete two elements starting from index 1
+// and add three elements.
+strArr.splice(1, 1, "Geeks", "Geeks1", "Geeks2");
+
+console.log(strArr);
