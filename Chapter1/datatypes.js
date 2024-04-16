ba39dfa8931a8fd3c53c61e267a8fbc02edcb26b
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-04-16 20:43:58 +0530

    Added dataypes in JS

---
+++ Chapter1/datatypes.js
@@ -0,0 +1,41 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2024-04-16");
+
+
+//null
+let a = null;
+
+// undefined
+
+let b;
+console.log(length);
+console.log(weight);
+console.log(color);
+console.log(lastName);
+console.log(x);
+console.log(y);
+console.log(person.lastName);
+console.log(date);
+console.log(a);
+console.log(b);
+
+
+
